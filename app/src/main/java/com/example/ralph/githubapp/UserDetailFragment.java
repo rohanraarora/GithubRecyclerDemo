947510d69a20d17b0d06c3224d83351b35513f1d
Rohan <rohanraarora@gmail.com>
2018-03-24 13:51:05 +0530

    Add detail fragment

--- app/src/main/java/com/example/ralph/githubapp/UserDetailFragment.java
+++ app/src/main/java/com/example/ralph/githubapp/UserDetailFragment.java
@@ -4,10 +4,14 @@ package com.example.ralph.githubapp;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 
@@ -30,6 +34,8 @@ public class UserDetailFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
+        setHasOptionsMenu(true);
         View view =  inflater.inflate(R.layout.fragment_user_detail, container, false);
 
         avatarImageView = view.findViewById(R.id.avatar);
@@ -47,4 +53,18 @@ public class UserDetailFragment extends Fragment {
         return view;
     }
 
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.menu_detail,menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.delete){
+            Toast.makeText(getContext(),"Delete",Toast.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
